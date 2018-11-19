fb0d1121b02bb5a54b254499c15bd88468bb05b1
CB_ATL_113_Smile_Rodnez <cb_atl_113_smile_rodnez@MacBook-Air-9.local>
2018-11-19 15:08:51 -0500

    forgot to commit now im screwed

--- client/src/components/Pikmin.js
+++ client/src/components/Pikmin.js
@@ -1,40 +1,46 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import axios from 'axios'
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
 
-const PikminStyles = styled.div`
-  display: flex;
-  position: relative;
-  flex-direction: column;
-  width: 200px;
-  height: 200px;
-  background: #f1faee;
-  margin: 10px 0;
-  button {
-    position: absolute;
-    top: 5px;
-    right: 10px;
-  }
 
-  input,
-  textarea {
-    background-color: transparent;
-    border: none;
-  }
+// const PikminStyles = styled.div`
+//   display: flex;
+//   position: relative;
+//   flex-direction: column;
+//   width: 200px;
+//   height: 200px;
+//   background: #f1faee;
+//   margin: 10px 0;
+//   button {
+//     position: absolute;
+//     top: 5px;
+//     right: 10px;
+//   }
 
-  input {
-    height: 30%;
-    font-size: 1.3rem;
-  }
-  textarea {
-    height: 70%;
-  }
-`
+//   input,
+//   textarea {
+//     background-color: transparent;
+//     border: none;
+//   }
+
+//   input {
+//     height: 30%;
+//     font-size: 1.3rem;
+//   }
+//   textarea {
+//     height: 70%;
+//   }
+// `
 
 class Pikmin extends Component {
   state = {
+    imageUrl: '',
     pikminName: '',
-      type: ''
+    type: '',
+    level: '',
+    weakness: '',
   }
 
   componentDidMount(){
@@ -67,18 +73,30 @@ class Pikmin extends Component {
 
   render() {
     return (
-      <PikminStyles>
+      div>
+      <Card>
+        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+    
+      {/* <PikminStyles> */}
         <input onBlur={this.handleUpdate}
           onChange={this.handleChange}
           type="text" name="title"
-          value={this.state.title}
+          value={this.state.pikminName}
         />
         <textarea onBlur={this.handleUpdate}
           onChange={this.handleChange}
           name="description" value={this.state.description}
         />
         <button onClick={this.handleDelete}>X</button>
-      </PikminStyles>
+      {/* </PikminStyles> */}
+      </div>
     )
   }
 }
